e98696e59e802fbc6c78aabd0a7b9ade48927111
Jackson Meneses <jacksonmeneses96@gmail.com>
2022-04-24 15:54:39 -0300

    Update projeto2.js

--- projeto2.js
+++ projeto2.js
@@ -6,59 +6,75 @@ console.log(
 );
 console.log();
 let opcoes = [0, 1, 2];
-let jogadas = +prompt("Quantas vezes você quer jogar? ");
 let wins = 0;
 let pcwins = 0;
 let draw = 0;
-if (jogadas != Number){
-  console.log('Digite um número válido!')
-  jogadas = +prompt("Quantas vezes você quer jogar? ");
-}
-for (let i = 0; i < jogadas; i++) {
-  let pc = opcoes[Math.floor(Math.random() * 3)];
-  user = +prompt("Escolha entre PEDRA(0), PAPEL(1), TESOURA(2): ");
-  if (isNaN(user) || user > 2) {
-    console.log("Digite uma opção válida!");
+var novamente = 1;
+while (novamente == 1) {
+  let jogadas = +prompt("Quantas vezes você quer jogar? ");
+  if (isNaN(jogadas)) {
+    console.log("Digite um número válido!");
+    jogadas = +prompt("Quantas vezes você quer jogar? ");
   }
-  if (user == 0 && pc == [0]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Pedra com Pedra da empate!");
-    draw++;
-  } else if (user == 0 && pc == [1]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Papel ganha de pedra!");
-    pcwins++;
-  } else if (user == 0 && pc == [2]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Pedra ganha de Tesoura!");
-    wins++;
-  } else if (user == 1 && pc == [0]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Papel ganha de pedra!");
-    wins++;
-  } else if (user == 1 && pc == [2]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Tesoura ganha de Papel!");
-    pcwins++;
-  } else if (user == 2 && pc == [0]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Pedra ganha de Tesoura!");
-    pcwins++;
-  } else if (user == 2 && pc == [1]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Tesoura ganha de Papel!");
-    wins++;
-  } else if (user == 1 && pc == [1]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Papel com papel da empate!");
-    draw++;
-  } else if (user == 2 && pc == [2]) {
-    console.log(`O Computador escolheu ${pc}`);
-    console.log("Tesoura com Tesoura da empate!");
-    draw++;
+  for (var i = 0; i < jogadas; i++) {
+    let pc = opcoes[Math.floor(Math.random() * 3)];
+    user = +prompt("Escolha entre PEDRA(0), PAPEL(1), TESOURA(2): ");
+    if (isNaN(user) || user > 2) {
+      console.log("Digite uma opção válida!");
+    }
+    if (user == 0 && pc == [0]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Pedra com Pedra da empate!");
+      draw++;
+    } else if (user == 0 && pc == [1]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Papel ganha de pedra!");
+      pcwins++;
+    } else if (user == 0 && pc == [2]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Pedra ganha de Tesoura!");
+      wins++;
+    } else if (user == 1 && pc == [0]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Papel ganha de pedra!");
+      wins++;
+    } else if (user == 1 && pc == [2]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Tesoura ganha de Papel!");
+      pcwins++;
+    } else if (user == 2 && pc == [0]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Pedra ganha de Tesoura!");
+      pcwins++;
+    } else if (user == 2 && pc == [1]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Tesoura ganha de Papel!");
+      wins++;
+    } else if (user == 1 && pc == [1]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Papel com papel da empate!");
+      draw++;
+    } else if (user == 2 && pc == [2]) {
+      console.log(`O Computador escolheu ${pc}`);
+      console.log("Tesoura com Tesoura da empate!");
+      draw++;
+    }
+  }
+  novamente = +prompt("Deseja jogar novamente? (0)Não (1)Sim: ");
+  if (novamente >= 2) {
+    console.log("Digite uma opção válida!");
+    novamente = +prompt("Deseja jogar novamente? (0)Não (1)Sim: ");
   }
 }
 console.log();
 console.log(`O usuario venceu ${wins} rodadas`);
 console.log(`O computador venceu ${pcwins} rodadas`);
 console.log(`Deu empate em ${draw} rodadas`);
+console.log();
+if (wins > pcwins) {
+  console.log(`!!!O Usuario venceu mais vezes e foi o grande CAMPEÃO!!!`);
+} else if (pcwins > wins) {
+  console.log("!!!O Computador venceu mais vezes e foi o grande CAMPEÃO!!!");
+} else {
+  console.log("Deu empate mais vezes do que os jogadores veceram!!!");
+}
